778149fd93ba8bb885413739c8783a656332e5d1
giangann <70595717+giangann@users.noreply.github.com>
2024-04-09 02:52:38 +0700

    [server] add default route

--- server/src/router/index.route.ts
+++ server/src/router/index.route.ts
@@ -4,4 +4,6 @@ import binanceRoute from "./binance.route";
 const indexRoute = express.Router();
 
 indexRoute.use("/binance", binanceRoute);
+
+indexRoute.use("/", (req, res) => res.json("this is express app bot api"));
 export default indexRoute;
