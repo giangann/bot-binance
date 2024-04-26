105f34aba553e95b8910dde31d4b8aac08994205
giangann <70595717+giangann@users.noreply.github.com>
2024-04-26 13:04:06 +0700

    test build 5

--- client/src/pages/account/Balance.tsx
+++ client/src/pages/account/Balance.tsx
@@ -1,15 +1,30 @@
 // @ts-nocheck
-import { Box, Skeleton, Typography } from "@mui/material";
+import CachedIcon from "@mui/icons-material/Cached";
+import { Box, IconButton, Skeleton, Stack, Typography } from "@mui/material";
 import { useContext, useEffect, useState } from "react";
 import { SocketContext } from "../../context/SocketContext";
+import { getApi } from "../../request/request";
 export type TUnknownObj = Record<string, string | number>;
 // type TCoin = { coin: string; amount: number; price: number; total: number };
 export const Balance = () => {
   const [balance, setBalance] = useState<TUnknownObj>({});
   const socket = useContext(SocketContext);
 
+  const fetchBalance = async () => {
+    const response = await getApi("user/acc-info-axios");
+    if (response.success) {
+      const { totalWalletBalance, availableBalance } = response.data;
+      setBalance({
+        ...balance,
+        totalWalletBalance,
+        availableBalance,
+      });
+    } else toast.error(response.error.message);
+  };
+
   useEffect(() => {
     socket?.on("ws-balance", (totalWalletBalance, availableBalance) => {
+      console.log("balance info", totalWalletBalance, availableBalance);
       setBalance({
         ...balance,
         totalWalletBalance,
@@ -22,7 +37,12 @@ export const Balance = () => {
   }, []);
   return (
     <Box>
-      <Typography variant="h6">Your balance</Typography>
+      <Stack direction="row" alignItems={"center"}>
+        <Typography variant="h6">Your balance</Typography>
+        <IconButton onClick={fetchBalance}>
+          <CachedIcon />
+        </IconButton>
+      </Stack>
       {balance.totalWalletBalance ? (
         <>
           <Typography>
