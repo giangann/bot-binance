94d79b27f5003a97fd61c3e7a4e81becca44b843
giangann <70595717+giangann@users.noreply.github.com>
2024-05-05 15:54:07 +0700

    [Server] log config

--- server/src/loaders/index.ts
+++ server/src/loaders/index.ts
@@ -2,19 +2,24 @@ import { createInterval } from "./create-interval";
 import { cronJobSchedule } from "./cron-job";
 import { connectDatabase } from "./db-connect";
 import { createHttpServer } from "./http-server";
+import { logger } from "./logger.config";
 import { subcribeAndForwardBinanceStream } from "./subcribe-binance-stream";
 import { createWebSocket } from "./ws-server";
 
 export const loadApp = async () => {
-  const httpServer = createHttpServer();
-  const wsServer = createWebSocket(httpServer);
-  global.wsServerGlob = wsServer;
-  subcribeAndForwardBinanceStream();
-  cronJobSchedule();
-  await connectDatabase();
-  createInterval();
-  return {
-    httpServer,
-    wsServer,
-  };
+  try {
+    const httpServer = createHttpServer();
+    const wsServer = createWebSocket(httpServer);
+    global.wsServerGlob = wsServer;
+    subcribeAndForwardBinanceStream();
+    cronJobSchedule();
+    await connectDatabase();
+    createInterval();
+    return {
+      httpServer,
+      wsServer,
+    };
+  } catch (err) {
+    logger.info(`The connection to database was failed with error: ${err}`);
+  }
 };
--- server/src/loaders/subcribe-binance-stream.ts
+++ server/src/loaders/subcribe-binance-stream.ts
@@ -30,29 +30,41 @@ const futurePriceStream = () => {
 };
 
 const testnetPriceStream = () => {
-  const baseWs = "wss://fstream.binancefuture.com";
-  const wsURL = `${baseWs}/stream?streams=!markPrice@arr/!ticker@arr`; // default is 3s interval
+  try {
+    const baseWs = "wss://fstream.binancefuture.com";
+    const wsURL = `${baseWs}/stream?streams=!markPrice@arr/!ticker@arr`; // default is 3s interval
 
-  const ws = new WebSocket(wsURL);
-  ws.on("open", () => {
-    console.log("WebSocket connection established.");
-  });
-  ws.on("message", (msg: any) => {
-    console.log("received new msg in testnet");
-    const messageString = msg.toString(); // Convert buffer to string
-    const messageJSON: TMarkPriceStream | TTickerPriceStream =
-      JSON.parse(messageString); // Parse string as JSON
+    const ws = new WebSocket(wsURL);
+    ws.on("open", () => {
+      console.log("WebSocket connection established.");
+    });
+    ws.on("message", (msg: any) => {
+      try {
+        console.log("received new msg in testnet");
+        // const msg2 = msg.alo.helo;
+        // global.wsServerGlob.emit("ws-balance", msg2, msg2);
+      } catch (err) {
+        console.log("err", err, " with msg: ", err.message);
+        throw err;
+      }
+      const messageString = msg.toString(); // Convert buffer to string
+      const messageJSON: TMarkPriceStream | TTickerPriceStream =
+        JSON.parse(messageString); // Parse string as JSON
 
-    // prepare emit
-    const emitMsg = binanceStreamToSymbolPrice(messageJSON);
-    global.wsServerGlob.emit("testnet-binance-stream-forward", emitMsg);
-  });
-  ws.on("error", (error: any) => {
-    console.error("WebSocket error:", error);
-  });
-  ws.on("close", () => {
-    console.log("WebSocket connection closed.");
-  });
+      // prepare emit
+      const emitMsg = binanceStreamToSymbolPrice(messageJSON);
+      global.wsServerGlob.emit("testnet-binance-stream-forward", emitMsg);
+    });
+
+    ws.on("error", (error: any) => {
+      console.error("WebSocket error:", error);
+    });
+    ws.on("close", () => {
+      console.log("WebSocket connection closed.");
+    });
+  } catch (err) {
+    throw err;
+  }
 };
 
 export const subcribeAndForwardBinanceStream = () => {
