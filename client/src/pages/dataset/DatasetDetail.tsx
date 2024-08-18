573d2db3119c0198e04ebffcc07dfc414e71472c
giangann <70595717+giangann@users.noreply.github.com>
2024-08-18 23:09:24 +0700

    dataset edit + delete item combination, pending create new items

--- client/src/pages/dataset/DatasetDetail.tsx
+++ client/src/pages/dataset/DatasetDetail.tsx
@@ -1,7 +1,7 @@
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditNoteIcon from "@mui/icons-material/EditNote";
 import NotInterestedIcon from "@mui/icons-material/NotInterested";
-import { Box, Button, IconButton, Typography } from "@mui/material";
+import { Box, Button, CircularProgress, Dialog, IconButton, Stack, Typography } from "@mui/material";
 import dayjs from "dayjs";
 import { useCallback, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
@@ -9,55 +9,81 @@ import { toast } from "react-toastify";
 import { BackToHomePage } from "../../components/BackHome/BackToHomePage";
 import { BasicTable } from "../../components/Table/BasicTable";
 import { StrictField } from "../../components/Table/Customtable";
-import { getApi, patchApi } from "../../request/request";
+import { deleteApi, getApi, patchApi } from "../../request/request";
 import { IDatasetCreate, IDatasetEntity, IDatasetItemRecord, IDatasetItemUpdate, IDatasetUpdate } from "../../shared/types/dataset";
-import { useFieldArray, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { BaseInput } from "../../components/Input";
 
 export const DatasetDetail = () => {
   const [dataset, setDataset] = useState<IDatasetEntity | null>(null);
   const [isEdit, setIsEdit] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [openCfDialog, setOpenCfDialg] = useState(false);
+  const [dialogHandlers, setDialogHandlers] = useState<{ onConfirm: () => void; onCancel: () => void }>({
+    onConfirm: () => {},
+    onCancel: () => {},
+  });
   const datasetItems = dataset?.dataset_items ?? [];
+  const params = useParams();
+  const datasetId = parseInt(params?.id ?? "0");
 
   const toEditMode = () => setIsEdit(true);
   const toViewMode = () => setIsEdit(false);
   const onToggle = isEdit ? toViewMode : toEditMode;
 
-  const params = useParams();
-  const datasetId = parseInt(params?.id ?? "0");
+  const fetchDatasets = useCallback(async () => {
+    const response = await getApi<IDatasetEntity>(`dataset/${datasetId}`);
+    if (response.success) {
+      setDataset(response.data);
+    } else toast.error(response.error.message);
+  }, [datasetId]);
 
-  const onDeleteItem = (id: number) => {
-    alert(`Item ${id} deleted !`);
+  const onDeleteItem = async (itemId: number): Promise<boolean> => {
+    return new Promise((resolve) => {
+      setOpenCfDialg(true);
+
+      const handleConfirmDelete = async () => {
+        try {
+          setIsDeleting(true);
+
+          const response = await deleteApi(`dataset-item/${itemId}`);
+          if (response.success) {
+            toast.success("Delete succeeded!");
+            fetchDatasets();
+            resolve(true);
+          } else {
+            toast.error(response.error.message);
+            resolve(false);
+          }
+        } catch (error: any) {
+          toast.error(error.message);
+        } finally {
+          setIsDeleting(false);
+          setOpenCfDialg(false);
+        }
+      };
+
+      const handleCancelDelete = () => {
+        setOpenCfDialg(false);
+        resolve(false);
+      };
+
+      setDialogHandlers({ onConfirm: handleConfirmDelete, onCancel: handleCancelDelete });
+    });
   };
 
-  // const fetchDatasets = useCallback(async () => {
-  //   const response = await getApi<IDatasetEntity>(`dataset/${datasetId}`);
-  //   if (response.success) {
-  //     setDataset(response.data);
-  //   } else toast.error(response.error.message);
-  // }, [datasetId]);
-
-  const fetchDatasets = useCallback(async () => {
-    async function fetchDetail(id: number) {
-      const response = await getApi<IDatasetEntity>(`dataset/${id}`);
-      if (response.success) {
-        setDataset(response.data);
-      } else toast.error(response.error.message);
+  const handleDeleteClick = async (itemId: number) => {
+    const shouldDelete = await onDeleteItem(itemId);
+    if (shouldDelete) {
+      // Additional logic after successful deletion, if needed
     }
-    fetchDetail(datasetId);
-  }, [datasetId]);
+  };
 
   const onSaveEdit = async (_value: IDatasetCreate | IDatasetUpdate) => {
     try {
-      // get statistic
       const { isDirty, dirtyFields } = formState;
+      const patchParams: IDatasetCreate | IDatasetUpdate = { id: datasetId };
 
-      // initial request params
-      const patchParams: IDatasetCreate | IDatasetUpdate = {
-        id: datasetId,
-      };
-
-      // add dirty field to params
       const dirtyFieldsKeys = Object.keys(dirtyFields);
       for (const key of dirtyFieldsKeys) {
         if (key !== "dataset_items") {
@@ -68,34 +94,27 @@ export const DatasetDetail = () => {
         }
       }
 
-      // the dataset_items is except and get handle below
-      // filter out null el
       const updatedItems = dirtyFields.dataset_items
         ?.map((item, index) => {
           const newObject: IDatasetItemUpdate = { id: index };
-          // get all keys have modify
           const itemKeys = Object.keys(item);
           for (const key of itemKeys) {
             // @ts-ignore
-            const value = getValues(`dataset_items.${index}.${key}`); // get new value
-            const newValue = value || null; // validate if "" => null
+            const value = getValues(`dataset_items.${index}.${key}`);
+            const newValue = value || null;
             // @ts-ignore
             newObject[key] = newValue;
           }
-
           return newObject;
         })
         .filter((item) => item);
 
       if (updatedItems) patchParams.dataset_items = updatedItems;
 
-      console.log({ isDirty, dirtyFields, updatedItems, patchParams });
-
-      // send patchParams with request
       const { id, ...otherParams } = patchParams;
       const response = await patchApi(`dataset/${datasetId}`, otherParams);
 
-      if (response.success) toast.error("Update success");
+      if (response.success) toast.success("Update success");
       else toast.error(response.error.message);
     } catch (error: any) {
       toast.error(error.message);
@@ -112,35 +131,24 @@ export const DatasetDetail = () => {
       header: "Order",
       fieldKey: "order",
       render: ({ id, order }) => {
-        return (
-          <>
-            {!isEdit && <Typography>{order}</Typography>}
-            {isEdit && <BaseInput {...register(`dataset_items.${id}.order`)} defaultValue={order} />}
-          </>
-        );
+        return !isEdit ? <Typography>{order}</Typography> : <BaseInput {...register(`dataset_items.${id}.order`)} defaultValue={order} />;
       },
     },
     {
       header: "Symbol",
       fieldKey: "symbol",
       render: ({ id, symbol }) => {
-        return (
-          <>
-            {!isEdit && <Typography>{symbol}</Typography>}
-            {isEdit && <BaseInput {...register(`dataset_items.${id}.symbol`)} defaultValue={symbol} />}
-          </>
-        );
+        return !isEdit ? <Typography>{symbol}</Typography> : <BaseInput {...register(`dataset_items.${id}.symbol`)} defaultValue={symbol} />;
       },
     },
     {
       header: "Ticker_price",
       fieldKey: "ticker_price",
       render: ({ id, ticker_price }) => {
-        return (
-          <>
-            {!isEdit && <Typography>{ticker_price}</Typography>}
-            {isEdit && <BaseInput {...register(`dataset_items.${id}.ticker_price`)} defaultValue={ticker_price} />}
-          </>
+        return !isEdit ? (
+          <Typography>{ticker_price}</Typography>
+        ) : (
+          <BaseInput {...register(`dataset_items.${id}.ticker_price`)} defaultValue={ticker_price} />
         );
       },
     },
@@ -148,11 +156,10 @@ export const DatasetDetail = () => {
       header: "Market_price",
       fieldKey: "market_price",
       render: ({ id, market_price }) => {
-        return (
-          <>
-            {!isEdit && <Typography>{market_price}</Typography>}
-            {isEdit && <BaseInput {...register(`dataset_items.${id}.market_price`)} defaultValue={market_price} />}
-          </>
+        return !isEdit ? (
+          <Typography>{market_price}</Typography>
+        ) : (
+          <BaseInput {...register(`dataset_items.${id}.market_price`)} defaultValue={market_price} />
         );
       },
     },
@@ -166,7 +173,7 @@ export const DatasetDetail = () => {
       width: 100,
       fieldKey: "id",
       render: ({ id }) => (
-        <IconButton onClick={() => onDeleteItem(id)}>
+        <IconButton onClick={() => handleDeleteClick(id)}>
           <DeleteIcon />
         </IconButton>
       ),
@@ -176,6 +183,7 @@ export const DatasetDetail = () => {
   useEffect(() => {
     fetchDatasets();
   }, [datasetId]);
+
   return (
     <Box>
       <BackToHomePage />
@@ -187,23 +195,40 @@ export const DatasetDetail = () => {
             Edit
           </Button>
         )}
-
         {isEdit && (
           <Button onClick={onToggle} variant="outlined" color="inherit" startIcon={<NotInterestedIcon />} sx={{ mr: 2 }}>
             Cancel
           </Button>
         )}
-        {isEdit && <></>}
         <Button type="submit" variant="contained" color="warning" disabled={!isEdit || !formState.isDirty}>
           Save
         </Button>
 
-        <>
-          {!isEdit && <Typography>{dataset?.name ?? "No data"}</Typography>}
-          {isEdit && <BaseInput {...register("name")} defaultValue={dataset?.name} />}
-        </>
+        {!isEdit ? <Typography>{dataset?.name ?? "No data"}</Typography> : <BaseInput {...register("name")} defaultValue={dataset?.name} />}
+
         <BasicTable data={datasetItems} fields={fields} />
       </Box>
+
+      <Dialog open={openCfDialog} onClose={dialogHandlers.onCancel}>
+        <Box p={4}>
+          <Typography variant="h6">Xác nhận xóa?</Typography>
+          <Box mb={2} />
+
+          <Stack direction="row" spacing={1} justifyContent={"flex-end"} alignItems={"center"}>
+            <Button onClick={dialogHandlers.onCancel} variant="contained">
+              Close
+            </Button>
+            <Button
+              onClick={dialogHandlers.onConfirm}
+              startIcon={isDeleting && <CircularProgress color="inherit" size={14} />}
+              variant="outlined"
+              color="error"
+            >
+              Xóa
+            </Button>
+          </Stack>
+        </Box>
+      </Dialog>
     </Box>
   );
 };
