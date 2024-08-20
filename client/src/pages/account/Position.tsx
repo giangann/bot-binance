6a7ee8b2111799d7494d40b31d2be8adb499be6e
giangann <70595717+giangann@users.noreply.github.com>
2024-08-20 23:35:26 +0700

    close all positions

--- client/src/pages/account/Position.tsx
+++ client/src/pages/account/Position.tsx
@@ -1,17 +1,23 @@
 import CachedIcon from "@mui/icons-material/Cached";
+import { Box, Button, CircularProgress, Dialog, IconButton, Skeleton, Stack, Typography, styled } from "@mui/material";
 import { useCallback, useContext, useEffect, useState } from "react";
-import { getApi } from "../../request/request";
-import { TPosition } from "../../shared/types/position";
 import { toast } from "react-toastify";
+import { CustomTable, StrictField } from "../../components/Table/Customtable";
 import { SocketContext } from "../../context/SocketContext";
-import { Box, IconButton, Skeleton, Stack, Typography, styled } from "@mui/material";
-import { StrictField } from "../../components/Table/Customtable";
-import { CustomTable } from "../../components/Table/Customtable";
+import { getApi, postApi } from "../../request/request";
+import { TPosition } from "../../shared/types/position";
 import { filterPositionsNotZero, sortPositionByPnl, totalUnrealizedPnl } from "../../ultils/helper";
 
 export const Position = () => {
   const [isLoading, setIsLoading] = useState(false);
+  const [isClosingPos, setIsClosingPos] = useState(false);
+  const [openCfDialog, setOpenCfDialog] = useState(false);
   const [positions, setPositions] = useState<TPosition[]>([]);
+
+  const [dialogHandlers, setDialogHandlers] = useState<{ onConfirm: () => void; onCancel: () => void }>({
+    onConfirm: () => {},
+    onCancel: () => {},
+  });
 
   const filterZeroPositions = filterPositionsNotZero(positions);
   const sortedPositions = sortPositionByPnl(filterZeroPositions);
@@ -27,6 +33,41 @@ export const Position = () => {
     setIsLoading(false);
   }, []);
 
+  const onCloseAllLongPositions = async (): Promise<boolean> => {
+    return new Promise((resolve) => {
+      setOpenCfDialog(true);
+
+      const handleConfirmDelete = async () => {
+        try {
+          setIsClosingPos(true);
+
+          const response = await postApi<{ success: number; failure: number }>("my-binance/close-all-long-positions", {});
+          if (response.success) {
+            const { failure, success } = response.data;
+            toast.success(`${success}/${success + failure} positions closed`);
+            fetchPosition();
+            resolve(true);
+          } else {
+            toast.error(response.error.message);
+            resolve(false);
+          }
+        } catch (error: any) {
+          toast.error(error.message);
+        } finally {
+          setIsClosingPos(false);
+          setOpenCfDialog(false);
+        }
+      };
+
+      const handleCancelDelete = () => {
+        setOpenCfDialog(false);
+        resolve(false);
+      };
+
+      setDialogHandlers({ onConfirm: handleConfirmDelete, onCancel: handleCancelDelete });
+    });
+  };
+
   useEffect(() => {
     socket?.on("ws-position-info", (positions: TPosition[]) => {
       setPositions(positions);
@@ -34,6 +75,10 @@ export const Position = () => {
     return () => {
       socket?.off("ws-position-info");
     };
+  }, []);
+
+  useEffect(() => {
+    fetchPosition();
   }, []);
 
   const fields: StrictField<TPosition>[] = [
@@ -79,11 +124,16 @@ export const Position = () => {
   ];
   return (
     <Box>
-      <Stack direction="row" alignItems={"center"}>
-        <Typography variant="h6">Your position</Typography>
-        <IconButton onClick={fetchPosition}>
-          <CachedIcon />
-        </IconButton>
+      <Stack direction="row" alignItems={"flex-start"} justifyContent={"space-between"}>
+        <Stack direction="row" alignItems={"center"}>
+          <Typography variant="h6">Your position</Typography>
+          <IconButton onClick={fetchPosition}>
+            <CachedIcon />
+          </IconButton>
+        </Stack>
+        <Button onClick={onCloseAllLongPositions} variant="contained" color="warning">
+          Close All
+        </Button>
       </Stack>
       {isLoading && <Skeleton variant="rectangular" width={"100%"} height={"200px"} />}
       {!isLoading && (
@@ -92,6 +142,26 @@ export const Position = () => {
           <CustomTable fields={fields} data={sortedPositions} />
         </>
       )}
+      <Dialog open={openCfDialog} onClose={dialogHandlers.onCancel}>
+        <Box p={4}>
+          <Typography variant="h6">Xác nhận đóng các Long Positions?</Typography>
+          <Box mb={2} />
+
+          <Stack direction="row" spacing={1} justifyContent={"flex-end"} alignItems={"center"}>
+            <Button onClick={dialogHandlers.onCancel} variant="outlined">
+              Close
+            </Button>
+            <Button
+              onClick={dialogHandlers.onConfirm}
+              disabled={isClosingPos}
+              startIcon={isClosingPos && <CircularProgress color="inherit" size={14} />}
+              variant="contained"
+            >
+              Ok
+            </Button>
+          </Stack>
+        </Box>
+      </Dialog>{" "}
     </Box>
   );
 };
